64d7da859420416c1e6c091fa948e0567db38da0
Hazem-Yasin <hazem.yasin.1416@gmail.com>
2024-04-23 13:58:52 +0200

    started a new project & added two models
    added two destinations Destination and traveller

---
+++ GoodDeedsDestinationsAPI/GoodDeedsDestinationsAPI/Models/Destination.cs
@@ -0,0 +1,11 @@
+﻿namespace GoodDeedsDestinationsAPI.Models
+{
+    public class Destination
+    {
+        public int DestinationID { get; set; }
+        public string DestinationName { get; set; }
+
+        public ICollection<Traveller> Traveller { get; set; }
+
+    }
+}
---
+++ GoodDeedsDestinationsAPI/GoodDeedsDestinationsAPI/Models/Traveller.cs
@@ -0,0 +1,12 @@
+﻿namespace GoodDeedsDestinationsAPI.Models
+{
+    public class Traveller
+    {
+        public int TravellerID { get; set; }
+
+        public string TravellerName { get; set; }
+
+
+        public ICollection<Destination> Destination { get; set; }
+    }
+}
